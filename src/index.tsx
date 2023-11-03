bb3653caa99908f54af00008df2055dfa981717c
MishynMykhailo <misham18092001@gmail.com>
2023-11-03 18:41:21 +0200

    fixed

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/eng-lets-talk-audio/">
       <React.StrictMode>
         <App />
       </React.StrictMode>
