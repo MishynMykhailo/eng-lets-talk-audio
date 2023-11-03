a3e83aa61ed6a14d86ef9207b91256eb1c4ef464
MishynMykhailo <misham18092001@gmail.com>
2023-11-03 18:54:12 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.scss";
 import App from "./components/App/App";
@@ -11,10 +11,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter basename="/eng-lets-talk-audio/">
       <React.StrictMode>
         <App />
       </React.StrictMode>
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 );
